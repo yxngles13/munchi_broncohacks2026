7c0f2ac0defcbb37804fbe716a6c9a137482caf4
yxngles13 <yongthulawong@gmail.com>
2026-04-26 08:24:29 -0700

    fixed faulty email button

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -16,7 +16,7 @@ export default function LoginPage() {
         const { error } = await supabase.auth.signInWithOtp({
             email,
             options: {
-                emailRedirectTo: "http://localhost:3000/auth/callback",
+                emailRedirectTo: `${window.location.origin}/auth/callback`,
             }
         })
         if (!error) {
